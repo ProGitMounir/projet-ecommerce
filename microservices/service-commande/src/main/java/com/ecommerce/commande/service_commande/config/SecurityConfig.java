6b685fa94bf40df7041a05b3a5a3652f96e42f46
ProGitMounir <pro.mailmounir@gmail.com>
2025-02-22 10:39:11 +0100

    Initialisation de la structure du projet avec les dossiers microservices. Ajout des sous-dossiers : service-commande, service-produit, service-utilisateur. ainsi que la configuration du spring security

---
+++ microservices/service-commande/src/main/java/com/ecommerce/commande/service_commande/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.ecommerce.commande.service_commande.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .requestMatchers("/api/public").permitAll()  // Autoriser l'accès public à /api/public
+            .requestMatchers("/api/**").authenticated()  // Protéger tous les autres endpoints sous /api/**
+            .anyRequest().permitAll()  // Autoriser l'accès à tous les autres endpoints sans authentification
+            .and()
+            .httpBasic();  // Utiliser l'authentification de base (Basic Auth)
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
